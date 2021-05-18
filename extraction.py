6892e7545085160373c981377a4d9aec27130b96
trenowanieprogramowania <trenowanieprogramowania@gmail.com>
2021-05-18 15:40:16 +0200

    Working function with loc method

--- extraction.py
+++ extraction.py
@@ -101,12 +101,12 @@ def investigate_air_quality(input_data_frame: pd.DataFrame):
                 return 'Very good'
 
 
-def data_air_quality(input_data_frame: pd.DataFrame):
+def data_air_quality_without_loc_method(input_data_frame: pd.DataFrame):
     input_data_frame['air_quality'] = input_data_frame.apply(investigate_air_quality, axis=1)
 
-    print('------------------------Generating group_by result---------------------------------------')
-    mean_value = input_data_frame.groupby(['param_formula'], as_index=False)['value'].mean()
-    print(mean_value.to_string())
+    # print('------------------------Generating group_by result---------------------------------------')
+    # mean_value = input_data_frame.groupby(['param_formula'], as_index=False)['value'].mean()
+    # print(mean_value.to_string())
 
     return input_data_frame
 
@@ -115,42 +115,67 @@ def data_air_quality_with_loc_method(input_data_frame: pd.DataFrame):
     with open('air_quality_index.json', 'r') as investigated_source:
         investigated_compounds = json.load(investigated_source)
 
-        if input_data_frame['param_formula'] in investigated_compounds["Compound"].keys():
-            investigated_param_formula = input_data_frame['param_formula']
+        # print('********************************************************************')
+        # print(type(input_data_frame['param_formula']))
+        # print('********************************************************************')
+        # print(type(investigated_compounds["Compound"].keys()))
 
-            input_data_frame.loc[input_data_frame['value'] \
-                                 > investigated_compounds['Compound'][investigated_param_formula]['Very bad'], \
-                                 'air_quality'] \
-                = 'Very bad'
+        given_compounds = set(input_data_frame['param_formula'])
 
-            input_data_frame.loc[investigated_compounds['Compound'][investigated_param_formula]['Very bad'] \
-                                 >= input_data_frame['value'] \
-                                 > investigated_compounds['Compound'][investigated_param_formula]['Bad'], 'air_quality'] \
-                = 'Bad'
+        for currently_investigated_compound in given_compounds:
 
-            input_data_frame.loc[investigated_compounds['Compound'][investigated_param_formula]['Bad'] \
-                                 >= input_data_frame['value'] \
-                                 > investigated_compounds['Compound'][investigated_param_formula]['Satisfactory'], \
-                                 'air_quality'] \
-                = 'Satisfactory'
+            if currently_investigated_compound in investigated_compounds["Compound"].keys():
+                investigated_param_formula = currently_investigated_compound
 
-            input_data_frame.loc[investigated_compounds['Compound'][investigated_param_formula]['Satisfactory'] \
-                                 >= input_data_frame['value'] \
-                                 > investigated_compounds['Compound'][investigated_param_formula]['Moderate'], \
-                                 'air_quality'] \
-                = 'Moderate'
+                input_data_frame.loc[input_data_frame['value']
+                                     > investigated_compounds['Compound'][investigated_param_formula]['Very bad'], \
+                                     'air_quality'] \
+                    = 'Very bad'
 
-            input_data_frame.loc[investigated_compounds['Compound'][investigated_param_formula]['Moderate'] \
-                                 >= input_data_frame['value'] \
-                                 > investigated_compounds['Compound'][investigated_param_formula][
-                                     'Good'], 'air_quality'] \
-                = 'Good'
+                input_data_frame.loc[((input_data_frame['value']
+                                       > investigated_compounds['Compound'][investigated_param_formula]['Bad'])
+                                      &
+                                      (input_data_frame['value']
+                                       <=
+                                       investigated_compounds['Compound'][investigated_param_formula]['Very bad'])), \
+                                     'air_quality'] \
+                    = 'Bad'
 
-            input_data_frame.loc[investigated_compounds['Compound'][investigated_param_formula]['Good'] \
-                                 >= input_data_frame['value'] \
-                                 > investigated_compounds['Very good'][investigated_param_formula][
-                                     'Good'], 'air_quality'] \
-                = 'Very good'
+                input_data_frame.loc[((input_data_frame['value']
+                                       > investigated_compounds['Compound'][investigated_param_formula]['Satisfactory'])
+                                      &
+                                      (input_data_frame['value']
+                                       <=
+                                       investigated_compounds['Compound'][investigated_param_formula]['Bad'])), \
+                                     'air_quality'] \
+                    = 'Satisfactory'
+
+                input_data_frame.loc[((input_data_frame['value']
+                                       > investigated_compounds['Compound'][investigated_param_formula]['Moderate'])
+                                      &
+                                      (input_data_frame['value']
+                                       <=
+                                       investigated_compounds['Compound'][investigated_param_formula]['Satisfactory'])), \
+                                     'air_quality'] \
+                    = 'Moderate'
+
+                input_data_frame.loc[((input_data_frame['value']
+                                       > investigated_compounds['Compound'][investigated_param_formula]['Good'])
+                                      &
+                                      (input_data_frame['value']
+                                       <=
+                                       investigated_compounds['Compound'][investigated_param_formula]['Moderate'])), \
+                                     'air_quality'] \
+                    = 'Good'
+
+                input_data_frame.loc[((input_data_frame['value']
+                                       > investigated_compounds['Compound'][investigated_param_formula]['Very good'])
+                                      &
+                                      (input_data_frame['value']
+                                       <=
+                                       investigated_compounds['Compound'][investigated_param_formula]['Good'])), \
+                                     'air_quality'] \
+                    = 'Very good'
 
     return input_data_frame
 
@@ -164,8 +189,11 @@ outcome_data_frame = generating_data_frame(list_of_objects)
 
 # data_frame = data_transformation(outcome_data_frame)
 
-data_frame = data_air_quality_with_loc_method(outcome_data_frame)
+# data_frame = data_air_quality_without_loc_method(outcome_data_frame)
 
-print(data_frame.to_string())
+other_data_frame = data_air_quality_with_loc_method(outcome_data_frame)
+
+# print(data_frame.to_string())
+print(other_data_frame.to_string())
 
 # print(list(data_frame.columns.values))
