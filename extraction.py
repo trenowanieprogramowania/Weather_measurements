92a95114c92d843cef31fa06b41189882cd0a322
trenowanieprogramowania <trenowanieprogramowania@gmail.com>
2021-05-18 16:08:36 +0200

    Detected inconsistencies in function with loc method

--- extraction.py
+++ extraction.py
@@ -115,11 +115,6 @@ def data_air_quality_with_loc_method(input_data_frame: pd.DataFrame):
     with open('air_quality_index.json', 'r') as investigated_source:
         investigated_compounds = json.load(investigated_source)
 
-        # print('********************************************************************')
-        # print(type(input_data_frame['param_formula']))
-        # print('********************************************************************')
-        # print(type(investigated_compounds["Compound"].keys()))
-
         given_compounds = set(input_data_frame['param_formula'])
 
         for currently_investigated_compound in given_compounds:
@@ -185,15 +180,30 @@ def data_air_quality_with_loc_method(input_data_frame: pd.DataFrame):
 
 list_of_objects = data_extraction(1)
 
-outcome_data_frame = generating_data_frame(list_of_objects)
+outcome_data_frame1 = generating_data_frame(list_of_objects)
+outcome_data_frame2 = generating_data_frame(list_of_objects)
 
 # data_frame = data_transformation(outcome_data_frame)
 
-# data_frame = data_air_quality_without_loc_method(outcome_data_frame)
+data_frame1 = data_air_quality_without_loc_method(outcome_data_frame1)
+data_frame2 = data_air_quality_with_loc_method(outcome_data_frame2)
 
-other_data_frame = data_air_quality_with_loc_method(outcome_data_frame)
+other_data_frame1 = data_air_quality_without_loc_method(outcome_data_frame1)
+other_data_frame2 = data_air_quality_with_loc_method(data_frame2)
 
+# print(other_data_frame1.equals(other_data_frame2))
 # print(data_frame.to_string())
-print(other_data_frame.to_string())
+#print(other_data_frame1.to_string())
+#print('Other dataFrame------------------------------------------------------------------------------------------------------------')
+#print(other_data_frame2.to_string())
+
+print(other_data_frame1.tail(5))
+print('-----------------------')
+print(other_data_frame2.tail(5))
+
+print(other_data_frame1.equals(other_data_frame2))
+
+# print(len(data_frame1))
+# print(len(data_frame2))
 
 # print(list(data_frame.columns.values))
