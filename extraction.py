8bdfe288d74ef710c95c3a8284540a3e82794897
trenowanieprogramowania <trenowanieprogramowania@gmail.com>
2021-05-18 17:16:22 +0200

    Added time performance function

--- extraction.py
+++ extraction.py
@@ -1,7 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 import json
 import requests
+import time
 
 from measurement import Measurement
 
@@ -175,33 +178,69 @@ def data_air_quality_with_loc_method(input_data_frame: pd.DataFrame):
     return input_data_frame
 
 
+def plot_time_performance(number_of_samples: int, size_of_step: int, initial_step: int):
+    final_step = initial_step + size_of_step * number_of_samples
+
+    list_of_outcomes_loc_method = []
+    list_of_outcomes_no_loc_method = []
+
+    for current_step in range(initial_step, final_step , size_of_step):
+        list_of_items = data_extraction(current_step)
+
+        outcome_data_frame_no_loc = generating_data_frame(list_of_items)
+        outcome_data_frame_loc = generating_data_frame(list_of_items)
+
+        no_loc_starts = time.perf_counter()
+        data_frame_no_loc = data_air_quality_without_loc_method(outcome_data_frame_no_loc)
+        no_loc_ends = time.perf_counter()
+        outcome_no_loc = no_loc_ends - no_loc_starts
+
+        loc_starts = time.perf_counter()
+        data_frame_loc = data_air_quality_with_loc_method(outcome_data_frame_loc)
+        loc_ends = time.perf_counter()
+        outcome_loc = loc_ends - loc_starts
+
+        list_of_outcomes_no_loc_method.append(outcome_no_loc)
+        list_of_outcomes_loc_method.append(outcome_loc)
+
+    print(list_of_outcomes_no_loc_method)
+
+    list_of_arguments = np.linspace(0, 1, number_of_samples)
+
+    plt.plot(list_of_arguments, list_of_outcomes_no_loc_method, 'r', label='without loc method')
+    plt.plot(list_of_arguments, list_of_outcomes_loc_method, 'b', label='with_loc_method')
+
+    plt.show()
+
 ####################################################################################
 
 
-list_of_objects = data_extraction(1)
+plot_time_performance(number_of_samples=5, size_of_step=1, initial_step=1)
 
-outcome_data_frame1 = generating_data_frame(list_of_objects)
-outcome_data_frame2 = generating_data_frame(list_of_objects)
+# list_of_objects = data_extraction(1)
+
+# outcome_data_frame1 = generating_data_frame(list_of_objects)
+# outcome_data_frame2 = generating_data_frame(list_of_objects)
 
 # data_frame = data_transformation(outcome_data_frame)
 
-data_frame1 = data_air_quality_without_loc_method(outcome_data_frame1)
-data_frame2 = data_air_quality_with_loc_method(outcome_data_frame2)
+# data_frame1 = data_air_quality_without_loc_method(outcome_data_frame1)
+# data_frame2 = data_air_quality_with_loc_method(outcome_data_frame2)
 
-other_data_frame1 = data_air_quality_without_loc_method(outcome_data_frame1)
-other_data_frame2 = data_air_quality_with_loc_method(data_frame2)
+# other_data_frame1 = data_air_quality_without_loc_method(outcome_data_frame1)
+# other_data_frame2 = data_air_quality_with_loc_method(data_frame2)
 
 # print(other_data_frame1.equals(other_data_frame2))
 # print(data_frame.to_string())
-#print(other_data_frame1.to_string())
-#print('Other dataFrame------------------------------------------------------------------------------------------------------------')
-#print(other_data_frame2.to_string())
+# print(other_data_frame1.to_string())
+# print('Other dataFrame------------------------------------------------------------------------------------------------------------')
+# print(other_data_frame2.to_string())
 
-print(other_data_frame1.tail(5))
-print('-----------------------')
-print(other_data_frame2.tail(5))
+# print(other_data_frame1.tail(5))
+# print('-----------------------')
+# print(other_data_frame2.tail(5))
 
-print(other_data_frame1.equals(other_data_frame2))
+# print(other_data_frame1.equals(other_data_frame2))
 
 # print(len(data_frame1))
 # print(len(data_frame2))
