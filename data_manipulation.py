d6f44d9deeafd18ffb760440d204c96503be036a
trenowanieprogramowania <trenowanieprogramowania@gmail.com>
2021-05-31 15:40:13 +0200

    Drafted attempt on writing optimal loc function

--- data_manipulation.py
+++ data_manipulation.py
@@ -42,14 +42,100 @@ def data_air_quality_without_loc_method(input_data_frame: pd.DataFrame):
 
 
 def data_air_quality_with_loc_method(input_data_frame: pd.DataFrame):
-    with open('air_quality_index.json', 'r') as investigated_source:
-        investigated_compounds = json.load(investigated_source)
+    # with open('air_quality_index.json', 'r') as investigated_source:
+    #    investigated_compounds = json.load(investigated_source)
 
+    data_for_data_frame = {
+        "PM10": [
+            (150, float('inf')),
+            (110, 150),
+            (80, 110),
+            (50, 80),
+            (20, 50),
+            (0, 20)
+        ],
+
+        "PM2.5": [
+            (110, float('inf')),
+            (75, 110),
+            (55, 75),
+            (35, 55),
+            (13, 35),
+            (0, 13)
+        ],
+
+        "O3": [
+            (240, float('inf')),
+            (180, 240),
+            (150, 180),
+            (120, 150),
+            (70, 120),
+            (0, 70)
+        ],
+
+        "NO2": [
+            (400, float('inf')),
+            (200, 400),
+            (150, 200),
+            (100, 150),
+            (40, 100),
+            (0, 40)
+        ],
+
+        "SO2": [
+            (500, float('inf')),
+            (350, 500),
+            (200, 350),
+            (100, 200),
+            (50, 100),
+            (0, 50)
+        ],
+
+        "C6H6": [
+            (51, float('inf')),
+            (21, 51),
+            (16, 21),
+            (11, 16),
+            (6, 11),
+            (0, 6)
+        ],
+
+        "CO": [
+            (21, float('inf')),
+            (15, 21),
+            (11, 15),
+            (7, 11),
+            (3, 7),
+            (0, 3)
+        ]
+    }
+
+    # input_data_frame.loc[
+    #                        , 'air_quality']
+
+    pollution_table = pd.DataFrame(data_for_data_frame,
+                                   index=['Very bad', 'bad', 'Satisfactory', 'Moderate', 'Good', 'Very good'])
+
+    # print(pollution_table.index)
+
+    input_data_frame.loc[pollution_table[input_data_frame['param_formula']][0]
+                         < input_data_frame['value']
+                         <= pollution_table[input_data_frame['param_formula']][1],
+                         'air_quality'] = 'ok'
+
+
+    '''
+        pollution_table.index[pollution_table[input_data_frame['param_formula']][0]
+                                                                < input_data_frame['value']
+                                                                <= pollution_table[input_data_frame['param_formula']][1]
+                                                                ]
+    '''
+    return input_data_frame
+
+    '''
         given_compounds = set(input_data_frame['param_formula'])
 
         for currently_investigated_compound in given_compounds:
-            # print(input_data_frame['param_formula'])
-
             investigated_param_formula = currently_investigated_compound
 
             input_data_frame.loc[input_data_frame['value']
@@ -101,5 +187,4 @@ def data_air_quality_with_loc_method(input_data_frame: pd.DataFrame):
                                        investigated_compounds['Compound'][investigated_param_formula]['Good'])), \
                                      'air_quality'] \
                     = 'Very good'
-
-    return input_data_frame
+    '''
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from other_api_extraction import create_temperature_column, create_pressure_colu
 
 # plot_time_performance(number_of_samples=20, size_of_step=1, initial_step=1)
 
-list_of_objects = data_extraction(limited_number_of_samples=2)
+list_of_objects = data_extraction(limited_number_of_samples=1)
 
 outcome_data_frame1 = generating_data_frame(list_of_objects)
 # outcome_data_frame2 = generating_data_frame(list_of_objects)
@@ -19,7 +19,7 @@ outcome_data_frame1 = generating_data_frame(list_of_objects)
 data_frame1 = data_air_quality_without_loc_method(outcome_data_frame1)
 # data_frame2 = data_air_quality_with_loc_method(outcome_data_frame2)
 
-data_frame1_with_temperature = create_temperature_column(data_frame1)
+# data_frame1_with_temperature = create_temperature_column(data_frame1)
 
 # data_frame1_with_pressure = create_pressure_column(data_frame1_with_temperature)
 
@@ -31,11 +31,11 @@ data_frame1_with_temperature = create_temperature_column(data_frame1)
 
 # print(data_frame1.equals(data_frame2))
 # print(data_frame.to_string())
-print(data_frame1_with_temperature.to_string())
+# print(data_frame1_with_temperature.to_string())
 # print('Other dataFrame-------------------------------------------------------------------------------------')
 # print(data_frame2.to_string())
 
-present_aggregated_data(data_frame1)
+# present_aggregated_data(data_frame1)
 
 # print(data_frame1.tail(5))
 # print('-----------------------')
